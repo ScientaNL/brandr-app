b2e2dde856c800a5544e7853a395b79aa19ed834
j.m.dejager <j.m.dejager@syslogic.nl>
2019-04-08 15:19:33 +0200

    created website

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,52 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {NgModule} from '@angular/core';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {MenuMainComponent} from './menu-main/menu-main.component';
+import {PageHomeComponent} from './page-home/page-home.component';
+import {
+	MatButtonModule, MatCardModule,
+	MatFormFieldModule,
+	MatInputModule,
+	MatListModule, MatTableModule,
+	MatToolbarModule
+} from "@angular/material";
+import {PageAboutComponent} from './page-about/page-about.component';
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import {BRANDrModule} from "./brandr/brandr.module";
+import {BRANDrApiUrl} from "./brandr/brandr-api.service";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		MenuMainComponent,
+		PageHomeComponent,
+		PageAboutComponent
+	],
+	imports: [
+		BRANDrModule,
+		BrowserModule,
+		BrowserAnimationsModule,
+		FormsModule,
+		HttpClientModule,
+		MatToolbarModule,
+		MatFormFieldModule,
+		MatButtonModule,
+		MatCardModule,
+		AppRoutingModule,
+		MatListModule,
+		MatInputModule,
+		MatCardModule,
+		MatTableModule
+	],
+	providers: [
+		{ provide: BRANDrApiUrl, useValue: 'http://10.10.11.233:1337' }
+	],
+	bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
